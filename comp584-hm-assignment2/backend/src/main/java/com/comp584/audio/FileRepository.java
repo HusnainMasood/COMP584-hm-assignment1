3cc15a13dc416f9c6a2cd2911fe503f6810e3197
Husnain Mohammad Masood <95274047+HusnainMasood@users.noreply.github.com>
2023-12-21 16:45:47 -0800

    Add files via upload

---
+++ comp584-hm-assignment2/backend/src/main/java/com/comp584/audio/FileRepository.java
@@ -0,0 +1,7 @@
+package backend.src.main.java.com.comp584.audio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileRepository extends JpaRepository<FileEntity, Long> {
+
+}
---
+++ comp584-hm-assignment2/backend/src/main/java/com/comp584/audio/FileService.java
@@ -0,0 +1,7 @@
+package backend.src.main.java.com.comp584.audio;
+
+import java.util.List;
+
+public interface FileService {
+    List<FileEntity> getAllFiles();
+}
