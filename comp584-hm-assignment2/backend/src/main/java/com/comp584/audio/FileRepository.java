f3d35ec934d8a4612caf52a80640cb995ae4e6ee
Husnain Mohammad Masood <95274047+HusnainMasood@users.noreply.github.com>
2023-12-21 16:50:39 -0800

    Delete comp584-hm-assignment2 directory

--- comp584-hm-assignment2/backend/src/main/java/com/comp584/audio/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package backend.src.main.java.com.comp584.audio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<FileEntity, Long> {
-
-}
--- comp584-hm-assignment2/backend/src/main/java/com/comp584/audio/FileService.java
+++
@@ -1,7 +0,0 @@
-package backend.src.main.java.com.comp584.audio;
-
-import java.util.List;
-
-public interface FileService {
-    List<FileEntity> getAllFiles();
-}
